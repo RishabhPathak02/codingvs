b898355ae9de7d1c10764c1d6b391156f1a77ef2
RishabhPathak02 <rishabh06pathak@gmail.com>
2023-06-17 17:05:00 +0530

    unique number

--- c++/array/unigue.cpp
+++ c++/array/unigue.cpp
@@ -3,16 +3,18 @@ using namespace std;
 int main(){
     int array[]={2,3,1,3,2,4,1};
     int i,j;
-    for(i=0;i<10;i++){
-        for(j=i+1;j<10;j++){
-            if(array[i]=array[j]){
+    int size=7;
+    for(i=0;i<size;i++){
+        for(j=i+1;j<size;j++){
+            if(array[i]==array[j]){
                 array[i]=array[j]=-1;
             }
         }
     }
-    for(i=0;i<10;i++){
+    for(i=0;i<size;i++){
         if(array[i]>0){
-            cout<<array[i];
+            int uni=array[i];
+            cout<<uni;
         }
     }
     return 0;

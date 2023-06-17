c3e8c7734f0549f74810f249d8a37d8defdd0ed2
RishabhPathak02 <rishabh06pathak@gmail.com>
2023-06-17 14:08:56 +0530

    uy

---
+++ c++/array/uniguenumbers.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int array[]={2,3,1,3,2,4,1};
+    int i,j;
+    for(i=0;i<10;i++){
+        for(j=i+1;j<10;j++){
+            if(array[i]=array[j]){
+                array[i]=array[j]=-1;
+            }
+        }
+    }
+    for(i=0;i<10;i++){
+        if(array[i]>0){
+            cout<<array[i];
+        }
+    }
+    return 0;
+}
